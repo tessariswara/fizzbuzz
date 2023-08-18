aa7aa37de51ba29a219695c6dd647961d108778a
eca <109819512+tessariswara@users.noreply.github.com>
2023-08-18 20:00:25 +0700

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,17 +5,22 @@ button.addEventListener("click", function () {
   const input = document.getElementById("input");
   const value = parseInt(input.value);
   let out = value;
-  if (value % 3 === 0 && value % 5 === 0) {
-    out = "FizzBuzz";
-    console.log("FizzBuzz");
-  } else if (value % 3 === 0) {
-    out = "Fizz";
-    console.log("Fizz");
-  } else if (value % 5 === 0) {
-    out = "Buzz";
-    console.log("Buzz");
+  if (value === 0) {
+    console.log("uhuy");
+    output.innerHTML += "Invalid input" + "<br>";
   } else {
-    console.log(value);
+    if (value % 3 === 0 && value % 5 === 0) {
+      out = "FizzBuzz";
+      console.log("FizzBuzz");
+    } else if (value % 3 === 0) {
+      out = "Fizz";
+      console.log("Fizz");
+    } else if (value % 5 === 0) {
+      out = "Buzz";
+      console.log("Buzz");
+    } else {
+      console.log(value);
+    }
+    output.innerHTML += out + "<br>";
   }
-  output.innerHTML += out + "<br>";
 });
